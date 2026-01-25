893076101518969486befcfcb6429c46af021509
Luigi Bergozza <lubber.luigi@outlook.com>
2026-01-25 00:59:47 -0300

    chore(config): setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//npm install react-router-dom  =  biblioteca de roteamento, Criar rotas e navegar entre elas
+//npm install --save-dev @types/react-router-dom  =  pacote de declaração de tipo
+import { Home } from "./pages/home";
+import { Tracking } from "./pages/tracking";
+import { Cotação } from "./pages/cotacao";
+import { Contato } from "./pages/contato";
+import { Location } from "./pages/location";
 
-function App() {
-  const [count, setCount] = useState(0)
+// 💡 Defina o nome do seu repositório aqui
+const REPO_NAME = "centraldeatendimeto"; 
 
+// O basename é "/" localmente (DEV) e "/nome-do-repositorio/" no deploy (PROD)
+const BASE_NAME = 
+  import.meta.env.DEV 
+  //É uma variável booleana que o Vite injeta. Quando você roda dev e for true será "/" do contrario será o nome do repositório
+    ? "/" 
+    : `/${REPO_NAME}/`; // Agora aponta para /centraldeatendimeto/
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    // Adicione a propriedade 'basename' ao BrowserRouter
+    <BrowserRouter basename={BASE_NAME}>
+      <Routes>
+        {/* As rotas internas permanecem com a barra ('/') */}
+        <Route path="/" element={<Home />} />
+        <Route path="/tracking" element={<Tracking />} />
+        <Route path="/cotacao" element={<Cotação />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/location" element={<Location />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
